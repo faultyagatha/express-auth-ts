752ecaf8d76ef110552572de14d78e794c174ad7
faultyagatha <valeria.barvinska@gmail.com>
2020-12-27 12:32:28 +0100

    add loginController decorator

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import cookieSession from 'cookie-session';
 
 import { router } from './routes/loginRoutes';
+import { AppRouter } from './AppRouter';
+import './controllers/LoginController';
 
 const app = express();
 
@@ -9,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['ghoshghg'] }));
 app.use(router);
+//app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
